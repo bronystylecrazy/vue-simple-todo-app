a63bcf6a1c22bded0e70025cf94f79564a437867
Sirawit Pratoomsuwan <s36526@bj.ac.th>
2020-11-26 20:50:21 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>I'm Computer Science Student!</h1>
+  <HelloWorld msg="My name is Sirawit Pratoomsuwan!" />
+  <br />
+</template>
+
+<script>
+/**
+ * Author: Sirawit Pratoomsuwan
+ * Created at: September 19, 2020, 7:48:39 pm
+ */
+
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
